5662506d8eb23169a9a9e7c4ac51463ba9b9648f
vdblm <balazadehvahid@gmail.com>
2020-01-01 01:37:22 +0330

    models

--- agilagic/urls.py
+++ agilagic/urls.py
@@ -1,7 +1,7 @@
 """agilagic URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+    https://docs.djangoproject.com/en/3.0/topics/http/urls.py/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -10,12 +10,13 @@ Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+    1. Import the include() function: from django.urls.py import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls.py'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path(r'', include('web.urls'))
 ]
---
+++ web/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+
+# Create your models here.
+from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+
+class Admin(User):
+    pass
+
+
+class Customer(User):
+    pass
+
+
+class Seller(User):
+    credit = models.BigIntegerField()
+
+
+class Product(models.Model):
+    pass
+
+
+class Contract(models.Model):
+    seller = models.ForeignKey(Seller, on_delete=models.CASCADE)
+    description = models.TextField()
+    response = models.TextField()
+    is_signed = models.BooleanField()
+    profit_perc = models.FloatField(validators=[MinValueValidator(0), MaxValueValidator(100)])
+
+    def sign_contract(self, sign, response):
+        self.is_signed = sign
+        self.response = response
+
+    def propose_contract(self, seller, description, profit_perc):
+        self.seller = seller
+        self.description = description
+        self.profit_perc = profit_perc
+
+    def __unicode__(self):
+        return self.seller.first_name + " " + self.seller.last_name
