5180c39b22dcf3ee855c2d1ecad1b20bd68cb55f
Mahdi <mahdi124710@yahoo.com>
2020-01-03 00:11:34 +0330

    sign_up & sign_in backend and UI are connected CSS can't be loaded in this version!

--- web/urls.py
+++ web/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path(r'sign_up/customer/', views.sign_up, name='sign_up_customer')
+    path(r'home/', views.home, name='home'),
+    path(r'home/sign_up.html/', views.sign_up, name='sign_up'),
+    path(r'home/sign_in.html/', views.sign_in, name='sign_in')
 ]
--- web/views.py
+++ web/views.py
@@ -2,11 +2,31 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from .models import CustomerManager
+from .forms import SignUpForm, SignInForm
+
+
+@csrf_exempt
+def home(request):
+    # this shows the home page of the website
+    return render(request, 'web/index.html')
 
 
 @csrf_exempt
 def sign_up(request):
-    # TODO : the password field should be sent coded
+    # the sign up form is provided for the new user
+    return render(request, 'web/sign_up.html')
+
+
+@csrf_exempt
+def verify_sign_up(request):
+    # this should get the input data from sign up form and add the new user to database
     params = request.POST
     result = CustomerManager.sign_up_customer(params['username'], params['email'], params['password'])
-    return HttpResponse(result)
+    return HttpResponse('sign up completed')
+
+
+@csrf_exempt
+def sign_in(request):
+    # the login form is provided for the user
+    form = SignInForm()
+    return render(request, 'web/SignIn.html', {'form': form})
