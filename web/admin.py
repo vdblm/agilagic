a3f0322d2c6813bb2d6ccc31092ed4920a1f7e70
Mahdi <mahdi124710@yahoo.com>
2020-01-10 03:13:40 +0330

    "sign_in_do" page deleted permission automated

--- web/admin.py
+++ web/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 
-admin.site.register(Customer)
-admin.site.register(Seller)
 admin.site.register(Product)
 admin.site.register(Contract)
+admin.site.register(WebsiteUser)
