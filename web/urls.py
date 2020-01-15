2ae6defd5c8ed9c823a470b1f9af01237e095446
Mahdi <mahdi124710@yahoo.com>
2020-01-15 13:47:50 +0330

    charging account for seller and customer is added

--- web/urls.py
+++ web/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
     path(r'propose_contract.html/', views.propose_contract, name='propose_contract'),
     path(r'propose_contract_do.html/', views.propose_contract_do, name='propose_contract_do'),
     path(r'propose_product.html/', views.propose_product, name='propose_product'),
+    path(r'charge_account.html/', views.charge_account, name='charge_account'),
+
 
 ]
