696510df7996767d8a38d9490189f6435f5e0a3c
vdblm <balazadehvahid@gmail.com>
2020-02-17 13:34:35 +0330

    debug false

--- agilagic/settings.py
+++ agilagic/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'twabqs^2ci^_sy+sl205bdnuybp5hp_4eq(839=uf10&*7-$=#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['agilagic.herokuapp.com', 'localhost', '127.0.0.1']
 
